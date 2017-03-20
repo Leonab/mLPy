aadb7e08c359b14a0f4043ba1688950a74291c7e
Nabeel Khawar <leonabrocks@yahoo.com>
2017-03-20 17:04:57 +0530

    works

--- sentiment.py
+++ sentiment.py
@@ -9,22 +9,30 @@ from collections import Counter
 lemmatizer = WordNetLemmatizer()
 hm_lines = 10000000
 
-def create_lex(pos, neg):
-    lexicon = []
-    for fi in [pos,neg]:
-        with open(fi,'r') as f:
-            contents = f.readlines()
-            for l in contents[:hm_lines]:
-                all_words = word_tokenize(l)
-                lexicon += list(all_words)
-    lexicon = [lemmatizer.lemmatize(i) for i in lexicon]
-    w_counts = Counter(lexicon)
-    l2 = []
-    for w in w_counts:
-        if 1000 > w_counts[w] > 50:
-            l2.append(w)
-    print(len(l2))
-    return l2
+def create_lex(pos,neg):
+
+	lexicon = []
+	with open(pos,'r') as f:
+		contents = f.readlines()
+		for l in contents[:hm_lines]:
+			all_words = word_tokenize(l)
+			lexicon += list(all_words)
+
+	with open(neg,'r') as f:
+		contents = f.readlines()
+		for l in contents[:hm_lines]:
+			all_words = word_tokenize(l)
+			lexicon += list(all_words)
+
+	lexicon = [lemmatizer.lemmatize(i) for i in lexicon]
+	w_counts = Counter(lexicon)
+	l2 = []
+	for w in w_counts:
+		#print(w_counts[w])
+		if 1000 > w_counts[w] > 50:
+			l2.append(w)
+	print(len(l2))
+	return l2
 
 
 def handle(sample, lexicon, classification):
@@ -35,7 +43,7 @@ def handle(sample, lexicon, classification):
         for l in contents[:hm_lines]:
             current_words = word_tokenize(l)
             current_words = [lemmatizer.lemmatize(i) for i in current_words]
-            features = np.zeroes(len(lexicon))
+            features = np.zeros(len(lexicon))
             for word in current_words:
                 if word.lower() in lexicon:
                     index_value = lexicon.index(word.lower())
@@ -45,23 +53,22 @@ def handle(sample, lexicon, classification):
 
     return featureset
 
-def create_sets_labels(pos, neg, test_size):
-    lexicon = create_lex(pos,neg)
-    features = []
-    features += handle('pos.txt')
-    features += handle('neg.txt')
-    random.shuffle(features)
+def create_sets_labels(pos, neg, test_size=0.1):
+	lexicon = create_lex(pos,neg)
+	features = []
+	features += handle('pos.txt',lexicon,[1,0])
+	features += handle('neg.txt',lexicon,[0,1])
+	random.shuffle(features)
+	features = np.array(features)
 
-    features = np.array(features)
+	testing_size = int(test_size*len(features))
 
-    testing_size = int(test_size*len(features))
-    train_x = list(features[:,0][:-testing_size])
-    train_y = list(features[:,1][:-testing_size])
+	train_x = list(features[:,0][:-testing_size])
+	train_y = list(features[:,1][:-testing_size])
+	test_x = list(features[:,0][-testing_size:])
+	test_y = list(features[:,1][-testing_size:])
 
-    test_x = list(features[:,0][:-testing_size])
-    test_y = list(features[:,1][:-testing_size])
-
-    return train_x,train_y,test_x,test_y
+	return train_x,train_y,test_x,test_y
 
 if __name__ == '__main__':
     train_x,train_y,test_x,test_y = create_sets_labels('pos.txt','neg.txt')
