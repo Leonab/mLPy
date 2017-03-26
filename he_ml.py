c61149299d768c68c75a8d140c8e4e3c15c6ee9f
Nabeel Khawar <leonabrocks@yahoo.com>
2017-03-26 22:01:15 +0530

    96% :(

--- he_ml.py
+++ he_ml.py
@@ -5,6 +5,7 @@ from xgboost import plot_importance
 import matplotlib.pyplot as plt
 import math
 from sklearn import preprocessing
+from sklearn import cross_validation, metrics
 from sklearn.ensemble import AdaBoostClassifier
 from sklearn.tree import DecisionTreeClassifier
 import matplotlib.pyplot as plt
@@ -36,12 +37,14 @@ def handle(df):
     return df
 
 train = handle(train)
-train.drop(['pymnt_plan','desc','title','verification_status_joint'],1,inplace=True)
-train.fillna(-99999,inplace=True)
+train.drop(['desc','verification_status_joint'],1,inplace=True)
+train.fillna(-999999,inplace=True)
+#train.dropna(inplace=True)
 
 test = handle(test)
-test.drop(['pymnt_plan','desc','title','verification_status_joint'],1,inplace=True)
-test.fillna(-99999,inplace=True)
+test.drop(['desc','verification_status_joint'],1,inplace=True)
+test.fillna(-999999,inplace=True)
+#test.dropna(inplace=True)
 print("handling done")
 
 X = np.array(train.drop(['loan_status'],1).astype(float))
@@ -53,7 +56,8 @@ X_test = preprocessing.scale(X_test)
 
 # dt = DecisionTreeClassifier()
 #clf = AdaBoostClassifier(n_estimators=100, base_estimator=lr,learning_rate=1)
-clf = xgboost.XGBClassifier()
+clf = xgboost.XGBClassifier(learning_rate =0.01, n_estimators=1000, max_depth=5, min_child_weight=1, gamma=0, subsample=0.8,
+                            colsample_bytree=0.8, objective= 'binary:logistic', nthread=4, scale_pos_weight=1, seed=27)
 clf.fit(X,y)
 print("fit done")
 with open("xgb.pickle","wb") as f:
@@ -65,12 +69,12 @@ plot_importance(clf)
 plt.show()
 print("pickle opened")
 
-predictions = clf.predict_proba(X_test)
-print(predictions[:,1])
+predictions = clf.predict_proba(X_test)[:,1]
+print(predictions)
 
 submission = pd.DataFrame({
-        "member_id": test["member_id"],
-        "loan_status": predictions[:,1]
+        "loan_status": predictions,
+        "member_id": test["member_id"]
 })
 
 submission.to_csv("submission.csv", index=False)
